25539dca993716695b3dfdf5667b944b8f834568
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2023-01-02 19:44:35 +0800

    Remove unuse library

--- PFD/Cryptography/IPasswordService.cs
+++ PFD/Cryptography/IPasswordService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PFD.Cryptography
+﻿namespace PFD.Cryptography
 {
     internal interface IPasswordService
     {
