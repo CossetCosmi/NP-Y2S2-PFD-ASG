70ed3ced343d71559ac4b883c05f89a829b45c28
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2023-01-24 12:50:33 +0800

    Add authentication main page

---
+++ PFD/UI/Authentication/AuthenticationPage.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace PFD.UI.Authentication
+{
+    internal sealed partial class AuthenticationPage : Page
+    {
+        private bool IsSpeechVisible { get; set; } = false;
+
+        #region OnClick Handler
+        private void BtnMenu_Click(object sender, RoutedEventArgs e) => svAuthentication.IsPaneOpen = !svAuthentication.IsPaneOpen;
+
+        private void CbSpeechText_Click(object sender, RoutedEventArgs e)
+        {
+            IsSpeechVisible = !IsSpeechVisible;
+
+            if (IsSpeechVisible)
+                Show_TbSpeech();
+            else Hide_TbSpeech();
+        }
+        #endregion OnClick Handler
+
+        #region Show/Hide Control
+        private void Hide_TbSpeech() => tbSpeech.Visibility = Visibility.Collapsed;
+        private void Show_TbSpeech() => tbSpeech.Visibility = Visibility.Visible;
+        #endregion Show/Hide Control
+    }
+}
