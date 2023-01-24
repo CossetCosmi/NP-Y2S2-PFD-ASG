f787322f7a6085b42f329bd691c7e9d53742aa43
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2023-01-24 14:25:38 +0800

    Add code to request microphone permission

---
+++ PFD/Component/AudioCapturePermission.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Media.Capture;
+using Windows.UI.Popups;
+
+
+namespace PFD.Component
+{
+    internal class AudioCapturePermission
+    {
+        private static int NoCaptureDevicesHResult { get; } = -1072845856;
+
+        public async static Task<bool> RequestMicrophonePermission()
+        {
+            bool hasPermission = false;
+
+            try
+            {
+                await new MediaCapture().InitializeAsync(new MediaCaptureInitializationSettings
+                {
+                    StreamingCaptureMode = StreamingCaptureMode.Audio,
+                    MediaCategory = MediaCategory.Speech
+                });
+
+                hasPermission = true;
+            }
+            catch (TypeLoadException)
+            {
+                await new MessageDialog("Media player components are unavailable.").ShowAsync();
+            }
+            catch (UnauthorizedAccessException) { }
+            catch (Exception exception)
+            {
+                if (exception.HResult != NoCaptureDevicesHResult)
+                    throw;
+                else await new MessageDialog("No Audio Capture devices are present on this system.").ShowAsync();
+            }
+
+            return hasPermission;
+        }
+    }
+}
