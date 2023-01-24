280a9c89aa36c62ce395676eff56dc2059f17f60
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2023-01-24 14:24:22 +0800

    Delete main page ui

--- PFD/App.xaml.cs
+++ PFD/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PFD.UI.Authentication;
+using System;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
@@ -56,7 +57,7 @@ namespace PFD
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(AuthenticationPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
