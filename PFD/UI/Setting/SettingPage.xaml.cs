10b0a459b6325628b98867258d77b08361d4fe97
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2023-01-25 01:44:04 +0800

    Add setting page

---
+++ PFD/UI/Setting/SettingPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using PFD.Model;
+using PFD.Repository;
+using System;
+using System.Collections.Generic;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+namespace PFD.UI.Setting
+{
+    public sealed partial class SettingPage : Page
+    {
+        #region Repository
+        private AccountRepository AccountRepository { get; } = AccountRepository.Instance;
+        private CardRepository CardRepository { get; } = CardRepository.Instance;
+        #endregion Repository
+
+        #region Properties
+        private List<Account> AccountList { get; set; }
+        private List<Card> CardList { get; set; }
+        #endregion Properties
+
+        public SettingPage()
+        {
+            InitializeComponent();
+            InitializeAccountList();
+        }
+
+        private void InitializeAccountList() => AccountList = AccountRepository.FindAll();
+
+        private async void cbAccount_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cbAccount = sender as ComboBox;
+            Account account = cbAccount.SelectedItem as Account;
+
+            CardList = new List<Card>();
+
+            foreach (Card card in CardRepository.FindAll())
+                if (card.AccountId == account.Id)
+                    CardList.Add(card);
+
+            cbCard.IsEnabled = true;
+
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => Bindings.Update());
+        }
+
+        private void cbCard_SelectionChanged(object sender, SelectionChangedEventArgs e) { }
+    }
+}
