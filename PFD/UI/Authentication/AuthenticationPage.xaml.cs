79e69342ec3378d74c73384c56756ff00cc1f49b
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2023-01-25 12:52:37 +0800

    Modified authentication main page

--- PFD/UI/Authentication/AuthenticationPage.xaml.cs
+++ PFD/UI/Authentication/AuthenticationPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using PFD.Component;
+using PFD.UI.Setting;
 using System;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +13,29 @@ namespace PFD.UI.Authentication
 {
     internal sealed partial class AuthenticationPage : Page
     {
-        public static AuthenticationPage Current;
+        #region Listener
+        private Action<bool> SpeechTextStateListener { get; set; }
+        #endregion Listener
 
-        private SpeechRecognizer SpeechRecognizer;
+        #region Speech Enginer Properties
         private bool IsListening { get; set; } = false;
+        private SpeechRecognizer SpeechRecognizer;
         private StringBuilder SpeechTextBuilder { get; } = new StringBuilder();
+        #endregion
+
+        #region Static Properties
+        public static AuthenticationPage Current;
+        #endregion
 
         public AuthenticationPage()
         {
-            InitializeComponent();
             Current = this;
+
+            InitializeComponent();
+            SpeechTextStateListener = async IsEnabled => await Dispatcher.RunAsync(CoreDispatcherPriority.Low, () => tbSpeech.Visibility = IsEnabled ? Windows.UI.Xaml.Visibility.Visible : Windows.UI.Xaml.Visibility.Collapsed);
         }
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
             bool hasPermission = await AudioCapturePermission.RequestMicrophonePermission();
 
@@ -38,9 +49,14 @@ namespace PFD.UI.Authentication
             await StartRecognizing();
 
             fvContent.Navigate(typeof(Auth_Main));
+            SettingPage.Current.AddSpeechTextStateListener(SpeechTextStateListener);
         }
 
-        protected async override void OnNavigatedFrom(NavigationEventArgs e) => await ReleaseSpeechRecognizerResource();
+        protected override async void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            await ReleaseSpeechRecognizerResource();
+            SettingPage.Current.RemoveSpeechTextStateListener(SpeechTextStateListener);
+        }
 
         private async Task ReleaseSpeechRecognizerResource()
         {
@@ -52,7 +68,7 @@ namespace PFD.UI.Authentication
                     IsListening = false;
                 }
 
-                SpeechRecognizer.StateChanged -= Recognizer_StateChanged;
+                //SpeechRecognizer.StateChanged -= Recognizer_StateChanged;
                 SpeechRecognizer.ContinuousRecognitionSession.Completed -= RecognizerSession_Completed;
                 SpeechRecognizer.ContinuousRecognitionSession.ResultGenerated -= RecognizerSession_ResultGenerated;
                 SpeechRecognizer.HypothesisGenerated -= Recognizer_HypothesisGenerated;
@@ -76,7 +92,7 @@ namespace PFD.UI.Authentication
                 tbSpeech.Text = $"Grammar Compilation Failed: {result.Status}";
             }
 
-            SpeechRecognizer.StateChanged += Recognizer_StateChanged;
+            //SpeechRecognizer.StateChanged += Recognizer_StateChanged;
             SpeechRecognizer.ContinuousRecognitionSession.Completed += RecognizerSession_Completed;
             SpeechRecognizer.ContinuousRecognitionSession.ResultGenerated += RecognizerSession_ResultGenerated;
             SpeechRecognizer.HypothesisGenerated += Recognizer_HypothesisGenerated;
@@ -104,7 +120,7 @@ namespace PFD.UI.Authentication
             }
         }
 
-        private async void Recognizer_StateChanged(SpeechRecognizer recognizer, SpeechRecognizerStateChangedEventArgs e) => await Dispatcher.RunAsync(CoreDispatcherPriority.Low, () => tbMicrophoneStatus.Text = $"Microphone: {e.State}");
+        //private async void Recognizer_StateChanged(SpeechRecognizer recognizer, SpeechRecognizerStateChangedEventArgs e) => await Dispatcher.RunAsync(CoreDispatcherPriority.Low, () => tbMicrophoneStatus.Text = $"Microphone: {e.State}");
 
         private async void Recognizer_HypothesisGenerated(SpeechRecognizer recognizer, SpeechRecognitionHypothesisGeneratedEventArgs e) => await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => tbSpeech.Text = $"{SpeechTextBuilder} {e.Hypothesis.Text} ...");
 
